3870e659af137d30685567b96f578fa0ffb9e8d7
FedorYrkov <fedoryurkov@gmail.com>
2021-11-08 23:08:11 +0300

    hw4

---
+++ hw4/test/test_task_4.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import requests
+
+
+def test_task_4(url_and_code):
+    response = requests.get(url=url_and_code["url"])
+    assert response.status_code == url_and_code["status_code"]
