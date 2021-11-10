4108b2163924977f83b9f3e4c01723d66eee684a
FedorYrkov <fedoryurkov@gmail.com>
2021-11-10 22:35:11 +0300

    fix test_task_4

--- hw4/conftest.py
+++ hw4/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--url", action="store", default="https://ya.ru")
-    parser.addoption("--status_code", action="store", default=200)
+    parser.addoption("--status_code", action="store", default="200")
 
 
 @pytest.fixture
--- hw4/test/test_task_4.py
+++ hw4/test/test_task_4.py
@@ -4,4 +4,4 @@ import requests
 
 def test_task_4(url_and_code):
     response = requests.get(url=url_and_code["url"])
-    assert response.status_code == url_and_code["status_code"]
+    assert str(response.status_code) == url_and_code["status_code"]
